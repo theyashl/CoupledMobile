9fbc96b4a04f585d9af1ff95d3bc93ed3269cd1b
Yash Lohakare <yashlohakare48@outlook.com>
2023-05-18 23:43:15 +0530

    removed unwanted code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,27 +14,6 @@ public class Main {
         mobile1.makeCall();
         mobile2.makeCall();
         System.out.println("Card of this phone is Modifiable: "+mobile1.cMod());
-
-        // a4g5d4f5rt5
-//        String s = "a4g5d4f5rt5";
-//        char[] sa = s.toCharArray();
-//        int sum = 0;
-//        for(char c: sa){
-//            if(Character.isDigit(c)){
-//                sum = sum + Character.getNumericValue(c);
-//            }
-//        }
-/*
-        for (int i=0;i<s.length();i++){
-            try{
-                sum = sum + Integer.parseInt(String.valueOf(sa[i]));
-            }
-            catch (NumberFormatException e){
-                //pass
-            }
-        }
-
-        System.out.println(sum);*/
     }
 }
 
